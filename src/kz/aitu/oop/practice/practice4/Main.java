4c174680c9906633f9c4b2d84e93639befac6b87
galamshar <myrzagalyev.galymzhan@gmail.com>
2020-09-05 15:02:29 +0600

    Added new line #2

--- src/kz/aitu/oop/practice/practice4/Main.java
+++ src/kz/aitu/oop/practice/practice4/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("New branch #1");
+        System.out.println("New branch #2");
     }
 }
